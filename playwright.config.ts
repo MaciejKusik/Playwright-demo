2c8b2fbddcda962c5c14c797834458b0eaf869e0
Maciej Kusik <kusik.maciej@gmail.com>
2026-02-09 17:46:08 +0100

    CI report

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from '@playwright/test'
 export default defineConfig({
   testDir: './tests',
   timeout: 20000,
+  reporter: [['html', { open: 'never' }]],
   use: {
     baseURL: 'https://jsonplaceholder.typicode.com',
   },
